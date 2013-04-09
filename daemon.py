335e6145a7166398d7d4ed4038f82581b329ad34
Martolod Slaaf <mart.slaaf@gmail.com>
2013-04-10 01:06:50 +0400

    Some light of PEP

--- daemon.py
+++ daemon.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 
-
-#############################################################
-# Script: sighandler.py                                      
-# Description:                                               
-#   Demonstrate generic polymorphism example of how to setup and
-#   handle implemented signals through python.                  
-#                                                               
-# Author: Cody Lane                                             
-# Date: 12-4-2009                                               
-############################################################# 
-import sys, os, time, atexit
+import sys
+import os
+import time
+import atexit
 import signal
-class SignalHandler:                                           
-    SIGNALS = ()                                            
+
+class SignalHandler:
+        
+                                                   
+    SIGNALS = () 
+    
     def register(self, signum, callback):                                                                              
         self.SIGNALS += (SigAction(signum, callback), )                       
 
@@ -27,6 +24,8 @@ class SignalHandler:
         return "<Class:%s>" %(self.__class__.__name__)        
 
 class SigAction(SignalHandler):
+    
+    
     def __init__(self, signum, callback):                                                                  
         self.signum = signum                                                  
         self.callback = callback                                              
@@ -38,137 +37,132 @@ class SigAction(SignalHandler):
     def __repr__(self):
         return "<Class:%s signal:%s>" %(self.__class__.__name__, self.signum)
 
-##################
-#
-#Source:http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
-#
-##################
-		
 class Daemon:
-	
-	def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
-		self.stdin = stdin
-		self.stdout = stdout
-		self.stderr = stderr
-		self.pidfile = pidfile
-		
-	def MetaInit(self, sigdict):
-		self.SigDict = sigdict
-	
-	def daemonize(self):
-		"""
-		do the UNIX double-fork magic, see Stevens' "Advanced 
-		Programming in the UNIX Environment" for details (ISBN 0201563177)
-		http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-		"""
-		try: 
-			pid = os.fork() 
-			if pid > 0:
-				# exit first parent
-				sys.exit(0) 
-		except OSError, e: 
-			sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
-			sys.exit(1)
-	
-		# decouple from parent environment
-		os.chdir("/") 
-		os.setsid() 
-		os.umask(0) 
-	
-		# do second fork
-		try: 
-			pid = os.fork() 
-			if pid > 0:
-				# exit from second parent
-				sys.exit(0) 
-		except OSError, e: 
-			sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
-			sys.exit(1) 
-	
-		# redirect standard file descriptors
-		sys.stdout.flush()
-		sys.stderr.flush()
-		si = file(self.stdin, 'r')
-		so = file(self.stdout, 'a+')
-		se = file(self.stderr, 'a+', 0)
-		os.dup2(si.fileno(), sys.stdin.fileno())
-		os.dup2(so.fileno(), sys.stdout.fileno())
-		os.dup2(se.fileno(), sys.stderr.fileno())
-	
-		# write pidfile
-		atexit.register(self.delpid)
-		pid = str(os.getpid())
-		file(self.pidfile,'w+').write("%s\n" % pid)
-	
-	def delpid(self):
-		os.remove(self.pidfile)
-		
-	def signalassign(self):
-		assignee = SignalHandler()
-		for i in iter(self.SigDict):
-			assignee.register(i,self.SigDict[i])
+    
+    
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+        
+    def metaInit(self, sigdict):
+        self.sigDict = sigdict
+    
+    def daemonize(self):
+        """
+        do the UNIX double-fork magic, see Stevens' "Advanced 
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+        """
+        try: 
+            pid = os.fork() 
+            if pid > 0:
+                # exit first parent
+                sys.exit(0) 
+        except OSError, e: 
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+    
+        # decouple from parent environment
+        os.chdir("/") 
+        os.setsid() 
+        os.umask(0) 
+    
+        # do second fork
+        try: 
+            pid = os.fork() 
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0) 
+        except OSError, e: 
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1) 
+    
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+    
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile,'w+').write("%s\n" % pid)
+    
+    def delpid(self):
+        os.remove(self.pidfile)
+        
+    def signalAssign(self):
+        assignee = SignalHandler()
+        for i in iter(self.sigDict):
+            assignee.register(i,self.sigDict[i])
 
-	def start(self):
-		"""
-		Start the daemon
-		"""
-		# Check for a pidfile to see if the daemon already runs
-		try:
-			pf = file(self.pidfile,'r')
-			pid = int(pf.read().strip())
-			pf.close()
-		except IOError:
-			pid = None
-	
-		if pid:
-			message = "pidfile %s already exist. Daemon already running?\n"
-			sys.stderr.write(message % self.pidfile)
-			sys.exit(1)
-		
-		# Start the daemon
-		self.daemonize()
-		self.signalassign()
-		self.run()
-	
-	SigDict = {}
-	
-	def stop(self):
-		"""
-		Stop the daemon
-		"""
-		# Get the pid from the pidfile
-		try:
-			pf = file(self.pidfile,'r')
-			pid = int(pf.read().strip())
-			pf.close()
-		except IOError:
-			pid = None
-	
-		if not pid:
-			message = "pidfile %s does not exist. Daemon not running?\n"
-			sys.stderr.write(message % self.pidfile)
-			return # not an error in a restart
-		sys.stderr.write("Kirosava!")
-		# Try killing the daemon process
-		try:
-			while 1:
-				os.kill(pid, signal.SIGTERM)
-				time.sleep(0.1)
-		except OSError, err:
-			err = str(err)
-			if err.find("No such process") > 0:
-				if os.path.exists(self.pidfile):
-					os.remove(self.pidfile)
-			else:
-				print str(err)
-				sys.exit(1)
+    def start(self):
+        """
+        Start the daemon
+        """
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+    
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+        
+        # Start the daemon
+        self.daemonize()
+        self.signalAssign()
+        self.run()
+    
+    sigDict = {}
+    
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+    
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return # not an error in a restart
+            
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
 
-	def restart(self):
-		"""
-		Restart the daemon
-		"""
-		self.stop()
-		self.start()
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop()
+        self.start()
 
-	def run(self):
-		print "dummy"
+    def run(self):
+        print "dummy"
--- subd.py
+++ subd.py
@@ -1,57 +1,61 @@
 #!/usr/bin/env python
-import sys, time, signal
+import sys
+import time
+import signal
 import daemon
 import demonconfig
 
 StaticConfig = demonconfig.StatCon()
 
 class MyDaemon(daemon.Daemon):
-	def run(self):
-		StaticConfig.run()
+    
+    
+    def run(self):
+        StaticConfig.run()
  
 class DaemonConfigurator:
-	
-	def __init__(self,ourdaemon):
-		self.ourdaemon = ourdaemon
-	
-	def GetSignalsForDaemon(self):
-		localCon = demonconfig.SigFunctionsCon(self.ourdaemon)
-		SigDict={}
-		for Sig in dir(localCon):
-			if Sig[0:1]!='_':
-				SigDict[getattr(signal, Sig)]=getattr(localCon, Sig)
-		return SigDict
-		
-	def GetReactsForDaemon(self):
-		localCon = demonconfig.ReactFunctionCon(self.ourdaemon)
-		ReactDict={}
-		for React in dir(localCon):
-			if React[0:1]!='_':
-				ReactDict[React]=getattr(localCon, React)
-		return ReactDict
+    
+    
+    def __init__(self,ourdaemon):
+        self.ourdaemon = ourdaemon
+    
+    def getSignalsForDaemon(self):
+        localCon = demonconfig.SigFunctionsCon(self.ourdaemon)
+        sigDict={}
+        for sig in dir(localCon):
+            if sig[0:1]!='_':
+                sigDict[getattr(signal, sig)]=getattr(localCon, sig)
+        return sigDict
+        
+    def getReactsForDaemon(self):
+        localCon = demonconfig.ReactFunctionCon(self.ourdaemon)
+        reactDict={}
+        for react in dir(localCon):
+            if react[0:1]!='_':
+                reactDict[react]=getattr(localCon, react)
+        return reactDict
 
 
 if __name__ == "__main__":
-	
-	daemon = MyDaemon(StaticConfig.PidFile, StaticConfig.Inputter, StaticConfig.Outputter, StaticConfig.Errorer)
-	
-	configer = DaemonConfigurator(daemon)
-	SigDict = configer.GetSignalsForDaemon()
-	
-	daemon.MetaInit(SigDict)
-	
-	ReactDict = configer.GetReactsForDaemon()
-	
-	print len(sys.argv)
-	if len(sys.argv) > 1:
-		if sys.argv[1] in iter(ReactDict):
-			try:
-				ReactDict[sys.argv[1]](*sys.argv[2:len(sys.argv)])
-				sys.exit(0)
-			except TypeError, error:
-				print error
-				print StaticConfig.Help
-				sys.exit(2)
-		else:
-			print "usage: %s %s" % sys.argv[0] % str(self.ReactDict)
-			sys.exit(2)
+    
+    daemon = MyDaemon(StaticConfig.pidFile, StaticConfig.inputter, StaticConfig.outputter, StaticConfig.errorer)
+    
+    configer = DaemonConfigurator(daemon)
+    SigDict = configer.getSignalsForDaemon()
+    
+    daemon.metaInit(SigDict)
+    
+    ReactDict = configer.getReactsForDaemon()
+    
+    if len(sys.argv) > 1:
+        if sys.argv[1] in iter(ReactDict):
+            try:
+                ReactDict[sys.argv[1]](*sys.argv[2:len(sys.argv)])
+                sys.exit(0)
+            except TypeError, error:
+                print error
+                print StaticConfig.strHelp
+                sys.exit(2)
+        else:
+            print "usage: %s %s" % sys.argv[0] % str(self.ReactDict)
+            sys.exit(2)
